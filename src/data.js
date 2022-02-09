dd30e269df815f08a9df1ab4e658cc70b5800825
Mario Gómez <magv181091@gmail.com>
2022-02-09 14:20:01 -0500

    Added dummy data to test

---
+++ src/data.js
@@ -0,0 +1,176 @@
+const clothesMen = [
+    {
+        "gender" : "m",
+        "name" : "shirt1",
+        "type" : "shirt",
+        "price" : 25,
+        "size" : {
+            "xs" : 20,
+            "s" : 20,
+            "m" : 35,
+            "l" : 30,
+            "xl" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "shirt2",
+        "type" : "shirt",
+        "price" : 30,
+        "size" : {
+            "xs" : 20,
+            "s" : 20,
+            "m" : 35,
+            "l" : 30,
+            "xl" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "shirt3",
+        "type" : "shirt",
+        "price" : 28,
+        "size" : {
+            "xs" : 20,
+            "s" : 20,
+            "m" : 35,
+            "l" : 30,
+            "xl" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "pants1",
+        "type" : "pants",
+        "price" : 40,
+        "size" : {
+            "xs" : 20,
+            "s" : 20,
+            "m" : 35,
+            "l" : 30,
+            "xl" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "pants2",
+        "type" : "pants",
+        "price" : 35,
+        "size" : {
+            "xs" : 20,
+            "s" : 20,
+            "m" : 35,
+            "l" : 30,
+            "xl" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "pants3",
+        "type" : "pants",
+        "price" : 37,
+        "size" : {
+            "xs" : 20,
+            "s" : 20,
+            "m" : 35,
+            "l" : 30,
+            "xl" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "shoes1",
+        "type" : "shoes",
+        "price" : 60,
+        "size" : {
+            "8" : 20,
+            "9" : 20,
+            "10" : 35,
+            "11" : 30,
+            "12" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "shoes2",
+        "type" : "shoes",
+        "price" : 65,
+        "size" : {
+            "8" : 20,
+            "9" : 20,
+            "10" : 35,
+            "11" : 30,
+            "12" : 20
+        }
+    },
+    {
+        "gender" : "m",
+        "name" : "shoes3",
+        "type" : "shoes",
+        "price" : 55,
+        "size" : {
+            "8" : 20,
+            "9" : 20,
+            "10" : 35,
+            "11" : 30,
+            "12" : 20
+        }
+    }
+]
+
+exports.clothesMen = clothesMen;
+
+const newsletter = [
+    {
+        "first" : "Mario",
+        "last" : "Gomez",
+        "email" : "magv181091@gmail.com",
+        "birthdate" : "18/10/1991",
+        "offers" : true,
+        "nation" : true,
+        "news" : true
+    }
+]
+
+exports.newsletter = newsletter;
+
+const users = [
+    {
+        "first" : "Mario",
+        "last" : "Gomez",
+        "age" : "30",
+        "id" : "xxxxxxxx",
+        "email" : "magv181091@gmail.com",
+        "birthdate" : "18/10/1991",
+        "phone" : "3123456789",
+        "address" : "Calle 1 # 2 - 3"
+    },
+    {
+        "first" : "Natalia",
+        "last" : "Alfonso",
+        "age" : "30",
+        "id" : "xxxxxxxx",
+        "email" : "naty.alfonso@hotmail.com",
+        "birthdate" : "29/12/1991",
+        "phone" : "3001234567",
+        "address" : "Calle 1 # 2 - 3"
+    },
+]
+
+exports.users = users;
+
+const orders = [
+    {
+        "id" : 1,
+        "date" : "03/02/2022",
+        "content" : "clothes name",
+        "email" : "magv181091@gmail.com",
+        "phone" : "3123456789",
+        "address" : "Calle 1 # 2 - 3",
+        "name": "Mario Gómez",
+        "message" : "Please leave with doorman in case I am not home",
+        "status" : "created"
+    }
+]
+
+exports.orders = orders;
